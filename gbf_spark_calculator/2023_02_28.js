74cdc3a853a33d3839f3ed07371998dd41c89bff
ShilpaKrishnanUnni <shilpakrishnanunni@gmail.com>
2023-02-28 22:40:23 +0530

    now it works

--- gbf_spark_calculator/2023_02_28.js
+++ gbf_spark_calculator/2023_02_28.js
@@ -3,10 +3,11 @@ let button = document.getElementById('button_calculate');
 function calculateSpark() {
     let tix = parseInt(document.getElementById('tix').value);
     let ten_tix = parseInt(document.getElementById('ten_tix').value);
-    let crystals = parseFloat(document.getElementById('crystals').value);
-    let rolls = tix + 10*ten_tix + parseInt(crystals/300);
-    // let rolls = tix + 10*ten_tix
-    let perc = parseFloat(rolls / 300);
+    let crystals = parseInt(document.getElementById('crystals').value);
+    let rolls = tix + 10*ten_tix + Math.floor(crystals/300);
+    // let rolls = Math.floor(crystals/300);
+    let perc = parseFloat(rolls / 3).toFixed(2);
     document.getElementById('draws').innerHTML=rolls;
     document.getElementById('percentage').innerHTML=perc;
+    
 };
