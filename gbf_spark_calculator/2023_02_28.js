c3dbf5bb6763c0bc63bbe8a327b889e6396d2fe4
Shilpa Krishnan Unni <shilpakrishnan@teamioss.in>
2023-03-02 09:26:17 +0530

    added a placeholder chartjs graph

--- gbf_spark_calculator/2023_02_28.js
+++ gbf_spark_calculator/2023_02_28.js
@@ -1,3 +1,5 @@
+// Modern front-end applications often use JavaScript module bundlers, so we’ve picked Parcel as a nice zero-configuration build tool. We’re also installing Chart.js v4 and a JavaScript client for Cube , an open-source API for data apps we’ll use to fetch real-world data (more on that later).
+
 let button = document.getElementById('button_calculate');
 
 function calculateSpark() {
@@ -11,3 +13,24 @@ function calculateSpark() {
     document.getElementById('percentage').innerHTML=perc;
     
 };
+
+const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
